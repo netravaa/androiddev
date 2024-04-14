a70be6e2e5fa54cd1cf9952d7b41555f44585005
Владимир Соколов <sokolover.heh@gmail.com>
2024-04-14 22:54:23 +0300

    prac8

---
+++ Prac8/app/src/main/java/com/netrava/prac8/App.java
@@ -0,0 +1,13 @@
+package com.netrava.prac8;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        DynamicColors.applyToActivitiesIfAvailable(this);
+        super.onCreate();
+    }
+}
